ffc733ea637cd9b42a7ceda73c30903b752e6286
pengdahong1225 <2102533303@qq.com>
2024-03-18 11:49:16 +0800

    xxx

--- oj-online-backend/proto/install.sh
+++ oj-online-backend/proto/install.sh
@@ -4,6 +4,6 @@ protoc --go_out=. --go-grpc_out=. *.proto
 
 install *.go ../question-service/proto
 install *.go ../db-service/proto
-install *.go ../judge-service/proto
+#install *.go ../judge-service/proto
 
 rm -rf *.go
