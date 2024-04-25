dde408365cf9e615db8c4214f0fb87712f59c31e
pengdahong1225 <2102533303@qq.com>
2024-04-25 16:49:36 +0800

    删除judger

--- oj-online-backend/judge-service/services/judger/core/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -v /root/server/oj-online-server/oj-online-backend/judge-service/judgecore:/root/builder -it -u root builder-core /bin/bash
--- oj-online-backend/judge-service/services/judger/wrapper/wrapper.h
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-#include <dlfcn.h>
-#include <sys/types.h>
-
-#include "../core/src/runner.h"
-
