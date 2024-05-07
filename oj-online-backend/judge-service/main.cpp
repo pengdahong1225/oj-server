5a126a9633e9016376ff6cdcd70434bf9b2021fc
pengdahong1225 <2102533303@qq.com>
2024-05-07 22:32:18 +0800

    judge-service

--- oj-online-backend/judge-service/main.cpp
+++ oj-online-backend/judge-service/main.cpp
@@ -4,17 +4,33 @@
 
 #include "app/AppServer.h"
 #include "muduo/base/Logging.h"
+#include "muduo/base/LogFile.h"
 #include "muduo/net/EventLoop.h"
 #include <unistd.h>
+#include <libgen.h>
 
-void init() {
-    muduo::Logger::setLogLevel(muduo::Logger::DEBUG);
-    muduo::Logger::setLogFileName("app.log"); // 设置日志文件名
-    muduo::Logger::initialize(); // 初始化日志系统
+std::unique_ptr<muduo::LogFile> g_logFile;
+
+void outputFunc(const char *msg, int len) {
+//    fwrite(msg, 1, len, stdout);
+    g_logFile->append(msg, len);
+}
+
+void flushFunc() {
+//    fflush(stdout);
+    g_logFile->flush();
 }
 
 int main(int argc, char *argv[]) {
-    LOG_INFO << "pid = " << getpid();
+    // 初始化日志
+    char name[256] = {'\0'};
+    strncpy(name, argv[0], sizeof name - 1);
+    g_logFile.reset(new muduo::LogFile(::basename(name), 200 * 1000));
+    muduo::Logger::setOutput(outputFunc);
+    muduo::Logger::setFlush(flushFunc);
+    muduo::Logger::setLogLevel(muduo::Logger::DEBUG);
+
+    LOG_INFO << "master pid = " << getpid();
 
     // server
     muduo::net::EventLoop loop;
