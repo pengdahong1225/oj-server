9102cf06b9fa589f2a4ac8fcaf5b3fdc6358fd47
pengdahong1225 <2102533303@qq.com>
2024-05-18 14:47:15 +0800

    question-service

--- oj-online-backend/question-service/services/judgeClient/codec.go
+++ oj-online-backend/question-service/services/judgeClient/codec.go
@@ -1,12 +1,25 @@
 package judgeClient
 
-import pb "question-service/logic/proto"
+import (
+	"google.golang.org/protobuf/proto"
+	pb "question-service/logic/proto"
+)
 
 var kHeaderLen = 4
 
 func decode(data []byte) *pb.SSJudgeResponse {
-
+	response := &pb.SSJudgeResponse{}
+	err := proto.Unmarshal(data, response)
+	if err != nil {
+		return nil
+	}
+	return response
 }
-func encode(request *pb.SSJudgeRequest) []byte {
 
+func encode(request *pb.SSJudgeRequest) []byte {
+	ret, err := proto.Marshal(request)
+	if err != nil {
+		return nil
+	}
+	return ret
 }
