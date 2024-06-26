cd4847db93eac6893a9ab8131bcaf5eb1e008ddb
pengdahong1225 <2102533303@qq.com>
2024-06-26 23:57:03 +0800

    xx

--- build-env/build.sh
+++
@@ -1,6 +0,0 @@
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <input need build path>"
-    exit
-fi
-
-sudo docker run -v $*:/root/builder -it -u root registry.cn-chengdu.aliyuncs.com/aliyun_pdh_space/builder:v1.0.0 /bin/bash
