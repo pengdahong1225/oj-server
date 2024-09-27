e3f5f9a1e172d2d282f75a5589b04a76aedc8e98
pengdahong <2102533303@qq.com>
2024-09-27 06:05:58 +0000

    建库脚本

--- oj-online-backend/mysql-init/init_table.sh
+++ oj-online-backend/mysql-init/init_table.sh
@@ -4,12 +4,12 @@ MYSQL=/usr/bin/mysql
 
 db_host=127.0.0.1
 db_port=3306
-db_user=root
-db_pwd=1225Gkl_
 db_name=oj_online_server
 
+read -p "Enter your pwd for db: " db_pwd
+
 # 建库
-$MYSQL -h$db_host -P$db_port -u$db_user -p$db_pwd -e "create database if not exists $db_name default character set = utf8mb4;"
+$MYSQL -h$db_host -P$db_port -uroot -p$db_pwd -e "create database if not exists $db_name default character set = utf8mb4;"
 
 # 建表
 if [ $# -eq 0 ]; then
@@ -19,7 +19,7 @@ fi
 
 for SOURCE_FILE in "$@"
 do
-  $MYSQL -h$db_host -P$db_port -u$db_user -p$db_pwd -e "source $SOURCE_FILE;"
+  $MYSQL -h$db_host -P$db_port -uroot -p$db_pwd -e "source $SOURCE_FILE;"
   if [ $? -eq 0 ]
   then
   	echo "table_create done."
@@ -27,3 +27,4 @@ do
   	echo "table_create failed."
   fi
 done
+
