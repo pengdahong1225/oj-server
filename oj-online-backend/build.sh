e795189d01c3c826c380812b22919d81d9fc834c
pengdahong <2102533303@qq.com>
2024-09-27 08:06:27 +0000

    部署

---
+++ oj-online-backend/build.sh
@@ -0,0 +1,24 @@
+# 编译
+services=("./db-service" "./judge-service" "./question-service")
+
+cd ./app || { echo "无法进入目录 app"; exit 1; }
+
+# 遍历文件夹名
+for s in "${services[@]}"; do
+    path="$s"
+    # 进入文件夹
+    cd "$path" || { echo "无法进入目录 $path"; exit 1; }
+
+    # 执行命令
+    echo "在 $path 中执行 go mod tidy"
+    go mod tidy
+
+    echo "在 $path 中执行 go build -o service ./cmd"
+    go build -o service ./cmd
+
+    # 返回上级目录
+    cd ..
+done
+
+# 构建serverbase
+docker build -f dockerfile.serverbase -t serverbase:latest .
