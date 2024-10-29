f86891229ac6e56fcd390c4a7d7ef847438ba54d
pengdahong <2102533303@qq.com>
2024-10-29 18:32:55 +0800

    frontend

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    DescriptionItem: typeof import('./src/components/descriptionItem.vue')['default']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
--- frontend/src/views/problem/ProblemPage.vue
+++ frontend/src/views/problem/ProblemPage.vue
@@ -6,5 +6,69 @@ console.log(route.params)
 </script>
 
 <template>
-    <div>problem</div>
+    <div class="container">
+        <!-- 左边题目描述和编辑区域 -->
+        <div class="left">
+            <el-card class="problem-description" shadow="hover">
+                <template #header>
+                    <strong>A+B</strong>
+                </template>
+
+                <div class="title">
+                    Description
+                </div>
+                <div class="content">
+                    输入一个正整数
+                </div>
+            </el-card>
+        </div>
+
+        <!-- 右边题目information区域 -->
+        <div class="right">
+            <el-card shadow="hover">
+                <template #header>
+                    <strong>Information</strong>
+                </template>
+                <el-descriptions :column="1" border>
+                    <el-descriptions-item label="ID">1</el-descriptions-item>
+                    <el-descriptions-item label="Time Limit">18100000000</el-descriptions-item>
+                    <el-descriptions-item label="Memory Limit">Suzhou</el-descriptions-item>
+                    <el-descriptions-item label="IO Mode">Suzhou</el-descriptions-item>
+                    <el-descriptions-item label="Created By">Suzhou</el-descriptions-item>
+                    <el-descriptions-item label="Level">Suzhou</el-descriptions-item>
+                    <el-descriptions-item label="Tags">
+                        <el-tag size="small">School</el-tag>
+                    </el-descriptions-item>
+                </el-descriptions>
+            </el-card>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    width: 80%;
+    margin: auto;
+    align-items: flex-start;
+
+    /* 确保卡片顶部对齐，而不会拉伸高度 */
+    .left {
+        width: 85%;
+        border: 1px solid #e42518;
+        margin-right: 5px;
+
+        .title {
+            font-size: 21px;
+            font-weight: 500;
+            line-height: 30px;
+            padding: 5px 10px;
+        }
+    }
+
+    .right {
+        width: 15%;
+        margin-left: 5px;
+    }
+}
+</style>
